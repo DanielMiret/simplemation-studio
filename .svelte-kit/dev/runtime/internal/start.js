33d55a8c3c14ecc7b50b7f095037b7062e722125
Dantti <danielmiretn@gmail.com>
2021-12-21 18:54:27 -0300

    Updated Node Dependencies

--- .svelte-kit/dev/runtime/internal/start.js
+++ .svelte-kit/dev/runtime/internal/start.js
@@ -13,12 +13,14 @@ function scroll_state() {
 }
 
 /**
- * @param {Node | null} node
- * @returns {HTMLAnchorElement | SVGAElement | null}
+ * @param {Event} event
+ * @returns {HTMLAnchorElement | SVGAElement | undefined}
  */
-function find_anchor(node) {
-	while (node && node.nodeName.toUpperCase() !== 'A') node = node.parentNode; // SVG <a> elements have a lowercase name
-	return /** @type {HTMLAnchorElement | SVGAElement} */ (node);
+function find_anchor(event) {
+	const node = event
+		.composedPath()
+		.find((e) => e instanceof Node && e.nodeName.toUpperCase() === 'A'); // SVG <a> elements have a lowercase name
+	return /** @type {HTMLAnchorElement | SVGAElement | undefined} */ (node);
 }
 
 /**
@@ -99,7 +101,7 @@ class Router {
 
 		/** @param {MouseEvent|TouchEvent} event */
 		const trigger_prefetch = (event) => {
-			const a = find_anchor(/** @type {Node} */ (event.target));
+			const a = find_anchor(event);
 			if (a && a.href && a.hasAttribute('sveltekit:prefetch')) {
 				this.prefetch(get_href(a));
 			}
@@ -129,7 +131,7 @@ class Router {
 			if (event.metaKey || event.ctrlKey || event.shiftKey || event.altKey) return;
 			if (event.defaultPrevented) return;
 
-			const a = find_anchor(/** @type {Node} */ (event.target));
+			const a = find_anchor(event);
 			if (!a) return;
 
 			if (!a.href) return;
@@ -645,9 +647,7 @@ class Renderer {
 			}
 		}
 
-		if (navigation_result.reload) {
-			location.reload();
-		} else if (this.started) {
+		if (this.started) {
 			this.current = navigation_result.state;
 
 			this.root.$set(navigation_result.props);
@@ -656,23 +656,35 @@ class Renderer {
 			this._init(navigation_result);
 		}
 
-		if (!opts) {
-			await 0;
-		} else {
+		// opts must be passed if we're navigating...
+		if (opts) {
 			const { hash, scroll, keepfocus } = opts;
 
 			if (!keepfocus) {
+				getSelection()?.removeAllRanges();
 				document.body.focus();
 			}
 
-			const oldPageYOffset = pageYOffset;
+			const old_page_y_offset = Math.round(pageYOffset);
+			const old_max_page_y_offset = document.documentElement.scrollHeight - innerHeight;
+
 			await 0;
-			const maxPageYOffset = document.body.scrollHeight - innerHeight;
+
+			const new_page_y_offset = Math.round(pageYOffset);
+			const new_max_page_y_offset = document.documentElement.scrollHeight - innerHeight;
 
 			// After `await 0`, the `onMount()` function in the component executed.
-			// If there was no scrolling happening (checked via `pageYOffset`),
-			// continue on our custom scroll handling
-			if (pageYOffset === Math.min(oldPageYOffset, maxPageYOffset)) {
+			// Check if no scrolling happened on mount.
+			const no_scroll_happened =
+				// In most cases, we can compare whether `pageYOffset` changed between navigation
+				new_page_y_offset === Math.min(old_page_y_offset, new_max_page_y_offset) ||
+				// But if the page is scrolled to/near the bottom, the browser would also scroll
+				// to/near the bottom of the new page on navigation. Since we can't detect when this
+				// behaviour happens, we naively compare by the y offset from the bottom of the page.
+				old_max_page_y_offset - old_page_y_offset === new_max_page_y_offset - new_page_y_offset;
+
+			// If there was no scrolling, we run on our custom scroll handling
+			if (no_scroll_happened) {
 				const deep_linked = hash && document.getElementById(hash.slice(1));
 				if (scroll) {
 					scrollTo(scroll.x, scroll.y);
@@ -685,6 +697,9 @@ class Renderer {
 					scrollTo(0, 0);
 				}
 			}
+		} else {
+			// ...they will not be supplied if we're simply invalidating
+			await 0;
 		}
 
 		this.loading.promise = null;
